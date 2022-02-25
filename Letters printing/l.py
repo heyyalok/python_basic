dcaa1d2f1996c1f4848b36179d30f15be38480de
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-02-25 22:56:02 +0530

    Create l.py

---
+++ Letters printing/l.py
@@ -0,0 +1,8 @@
+#L
+for row2 in range (4):
+    for col2 in range (4):
+        if (row2 == 3 or col2 == 0) :
+            print('#',end=(''))
+        else:
+            print('',end=(''))
+    print()
